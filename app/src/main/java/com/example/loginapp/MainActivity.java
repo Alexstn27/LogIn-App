9399a17f07aa399988ef005d8e566c58f26889c8
Alexstn27 <alexuzu_mes@yahoo.com>
2019-07-18 19:22:28 +0300

    hide back button

--- app/src/main/java/com/example/loginapp/MainActivity.java
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -39,7 +39,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume ();
 
+        if (!Password.getText().toString().isEmpty()) {
+            Password.setText("");
+        }
 
+        if (!Name.getText().toString().isEmpty()) {
+            Name.setText("");
+        }
     }
 
     @Override
@@ -157,40 +163,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public class AESCrypt
-    {
-        private static final String ALGORITHM = "AES";
-        private static final String KEY = "1Hbfh667adfDEJ78";
 
-        public static String encrypt(String value) throws Exception
-        {
-            Key key = generateKey();
-            Cipher cipher = Cipher.getInstance(AESCrypt.ALGORITHM);
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            byte [] encryptedByteValue = cipher.doFinal(value.getBytes("utf-8"));
-            String encryptedValue64 = Base64.encodeToString(encryptedByteValue, Base64.DEFAULT);
-            return encryptedValue64;
-
-        }
-
-        public static String decrypt(String value) throws Exception
-        {
-            Key key = generateKey();
-            Cipher cipher = Cipher.getInstance(AESCrypt.ALGORITHM);
-            cipher.init(Cipher.DECRYPT_MODE, key);
-            byte[] decryptedValue64 = Base64.decode(value, Base64.DEFAULT);
-            byte [] decryptedByteValue = cipher.doFinal(decryptedValue64);
-            String decryptedValue = new String(decryptedByteValue,"utf-8");
-            return decryptedValue;
-
-        }
-
-        private static Key generateKey() throws Exception
-        {
-            Key key = new SecretKeySpec(AESCrypt.KEY.getBytes(),AESCrypt.ALGORITHM);
-            return key;
-        }
-    }
 
 
         //TODO:hide keyboard when click outside of EditText
